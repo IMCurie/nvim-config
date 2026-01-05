fb5f3ad304211490db895c6ec3bd321b81e8a908
IMCurie <imcurie.free@gmail.com>
2026-01-06 01:07:11 +0800

    Add key mappings for quick quit, save, and source commands

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap = true })
+vim.keymap.set("n", "<leader>q", "<cmd>q<CR>")
+vim.keymap.set("n", "<leader>w", "<cmd>w<CR>")
+vim.keymap.set("n", "<leader>o", "<cmd>so<CR>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
