fa6fa021c8eff5401a4a663b4ccb8d8c82201b4c
IMCurie <imcurie.free@gmail.com>
2026-03-17 15:43:03 +0800

    Remove toggleterm and switch-dir, rebind <C-\> to telescope buffers

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,7 +12,7 @@ return {
   end,
   keys = {
     {
-      "<C-b>",
+      "<C-\\>",
       function()
         local builtin = require("telescope.builtin")
         local ok, err = pcall(builtin.buffers)
